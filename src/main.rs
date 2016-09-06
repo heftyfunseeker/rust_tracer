b1e8c7c445ca2a84cfc99a5a3f9f17f79f7dc049
heftyfunseeker <niccosimone@gmail.com>
2016-09-05 19:56:11 -0700

    added math lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate rusty_math;
+
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
@@ -14,13 +16,16 @@ fn main() {
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let r: f32 = (i as f32) / nx as f32;
-            let g: f32 = (j as f32) / ny as f32;
-            let b: f32 = 0.2;
+            let v = rusty_math::Vec3 {
+                x: (i as f64) / (nx as f64),
+                y: (j as f64) / (ny as f64),
+                z: 0.2f64
+            };
 
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+            let ir = (255.99 * v.x) as i32;
+            let ig = (255.99 * v.y) as i32;
+            let ib = (255.99 * v.z) as i32;
+
 
             ppm_str.push_str(&format!("{} {} {}\n", ir, ig, ib));
         }
