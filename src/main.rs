597ae05181c004a213baf76a6026e808a9941a4e
heftyfunseeker <niccosimone@gmail.com>
2016-10-09 21:30:24 -0700

    let there be a sphere...well, just a circle for now

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,19 @@ use std::io::prelude::*;
 use std::path::Path;
 use rusty_math::*;
 
+fn hit_sphere(center: &Vec3, radius: f64, ray: &Ray) -> bool {
+    let offset_origin = &ray.origin - center;
+    let a = ray.dir.dot(&ray.dir);
+    let b = 2f64 * offset_origin.dot(&ray.dir);
+    let c = offset_origin.dot(&offset_origin) - radius * radius;
+    let discriminant = &b * &b - 4f64 * &a * &c;
+    return discriminant > 0f64;
+}
+
 fn color(ray: &Ray) -> Vec3 {
+    if hit_sphere(&Vec3::new(0f64,0f64,-1f64), 0.5f64, ray) {
+        return Vec3::new(1f64,0f64,0f64);
+    }
     let t = 0.5f64 * (ray.dir.normalize().y + 1.0f64);
     let white = Vec3::new(1f64, 1f64, 1f64);
     let blue = Vec3::new(0.5f64, 0.7f64, 1.0f64);
@@ -22,7 +34,7 @@ fn main() {
     let origin = Vec3::new(0f64,0f64,0f64);
 
     let mut r = Ray::new(origin, Vec3::new(0f64,0f64,0f64));
-    
+
     // write header
     let mut ppm_str = String::new();
     ppm_str.push_str(&format!("P3\n{} {}\n255\n", nx, ny));
