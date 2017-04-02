5fd86c6869126525bae7f181aa4648fe58cc28b3
heftyfunseeker <niccosimone@gmail.com>
2017-04-01 17:01:54 -0700

    added dielectrics

--- src/main.rs
+++ src/main.rs
@@ -22,56 +22,74 @@ use render_buffer::RenderBufferI32;
 mod renderer;
 
 fn main() {
-	// setup the world
-	// all materials must be declared before the renderlist references them
-	let lambertian_red = materials::Lambertian {albedo: Vec3::new(0.8f64, 0.3f64, 0.3f64)};
-	let lambertian_blue = materials::Lambertian {albedo: Vec3::new(0.8f64, 0.8f64, 0f64)};
-	let metal_1 = materials::Metal {albedo: Vec3::new(0.8f64, 0.6f64, 0.2f64), fuzziness: 0.8f64};
-	let metal_2 = materials::Metal {albedo: Vec3::new(0.8f64, 0.8f64, 0.8f64), fuzziness: 0.3f64};
-	let dielectric = materials::Dielectric {refractionIndex: 1.5f64};
+    // setup the world
+    // all materials must be declared before the renderlist references them
+    let lambertian_green = materials::Lambertian { albedo: Vec3::new(0.8f64, 0.8f64, 0.3f64) };
+    let lambertian_blue = materials::Lambertian { albedo: Vec3::new(0.1f64, 0.2f64, 0.5f64) };
+    let metal_1 = materials::Metal {
+        albedo: Vec3::new(0.8f64, 0.6f64, 0.2f64),
+        fuzziness: 0.35f64,
+    };
+    let dielectric = materials::Dielectric { refraction_index: 1.5f64 };
 
-	let mut world = RenderList::new();
-	let small_sphere = shapes::Sphere {center: Vec3::new(0f64, 0f64, -1f64), radius: 0.5f64};
-	let right_sphere  = shapes::Sphere {center: Vec3::new(-1f64, 0f64, -1f64), radius: 0.5f64};
-	let left_sphere  = shapes::Sphere {center: Vec3::new(1f64, 0f64, -1f64), radius: 0.5f64};
-	let big_sphere = shapes::Sphere {center: Vec3::new(0f64, -100.5f64, -1f64), radius: 100f64};
-	world.add_sphere(&small_sphere, &lambertian_red);
-	world.add_sphere(&big_sphere, &lambertian_blue);
-	world.add_sphere(&right_sphere, &dielectric);
-	world.add_sphere(&left_sphere, &metal_1);
-	let camera = Camera::new();
-	let mut output_buffer = RenderBufferI32::new(200, 100);
+    let mut world = RenderList::new();
+    let small_sphere = shapes::Sphere {
+        center: Vec3::new(0f64, 0f64, -1f64),
+        radius: 0.5f64,
+    };
+    let right_sphere1 = shapes::Sphere {
+        center: Vec3::new(-1f64, 0f64, -1f64),
+        radius: 0.5f64,
+    };
+    let right_sphere2 = shapes::Sphere {
+        center: Vec3::new(-1f64, 0f64, -1f64),
+        radius: -0.45f64,
+    };
+    let left_sphere = shapes::Sphere {
+        center: Vec3::new(1f64, 0f64, -1f64),
+        radius: 0.5f64,
+    };
+    let big_sphere = shapes::Sphere {
+        center: Vec3::new(0f64, -100.5f64, -1f64),
+        radius: 100f64,
+    };
+    world.add_sphere(&small_sphere, &lambertian_blue);
+    world.add_sphere(&big_sphere, &lambertian_green);
+    world.add_sphere(&right_sphere1, &dielectric);
+    world.add_sphere(&right_sphere2, &dielectric);
+    world.add_sphere(&left_sphere, &metal_1);
+    let camera = Camera::new();
+    let mut output_buffer = RenderBufferI32::new(200, 100);
 
-	// render
-	{
-		let render_settings = renderer::RenderSettings {
-			num_samples_per_pixel: 100,
-		};
-		// create the package to render
-		let mut render_package = renderer::RenderPackage {
-			render_list: &world,
-			camera: &camera,
-			output_buffer: &mut output_buffer,
-		};
+    // render
+    {
+        let render_settings = renderer::RenderSettings { num_samples_per_pixel: 100 };
+        // create the package to render
+        let mut render_package = renderer::RenderPackage {
+            render_list: &world,
+            camera: &camera,
+            output_buffer: &mut output_buffer,
+        };
 
-		renderer::render(&mut render_package, &render_settings);
-	}
+        renderer::render(&mut render_package, &render_settings);
+    }
 
     // write header
     let mut ppm_str = String::new();
-    ppm_str.push_str(&format!("P3\n{} {}\n255\n", output_buffer.width, output_buffer.height));
+    ppm_str.push_str(&format!("P3\n{} {}\n255\n",
+                              output_buffer.width,
+                              output_buffer.height));
 
-	let mut pixel_index:usize = 0;
-	while pixel_index < output_buffer.buffer.len()
-	{
-		let ir = output_buffer.buffer[pixel_index];
-		let ig = output_buffer.buffer[pixel_index + 1];
-		let ib = output_buffer.buffer[pixel_index + 2];
+    let mut pixel_index: usize = 0;
+    while pixel_index < output_buffer.buffer.len() {
+        let ir = output_buffer.buffer[pixel_index];
+        let ig = output_buffer.buffer[pixel_index + 1];
+        let ib = output_buffer.buffer[pixel_index + 2];
 
-	    ppm_str.push_str(&format!("{} {} {}\n", ir, ig, ib));
+        ppm_str.push_str(&format!("{} {} {}\n", ir, ig, ib));
 
-		pixel_index += 3;
-	}
+        pixel_index += 3;
+    }
 
     // write to file
     let path = Path::new("image.ppm");
@@ -81,9 +99,7 @@ fn main() {
         Ok(file) => file,
     };
     match file.write_all(ppm_str.as_bytes()) {
-        Err(why) => {
-            panic!("{}", why.description())
-        },
-        Ok(_) => println!("write to file successful")
+        Err(why) => panic!("{}", why.description()),
+        Ok(_) => println!("write to file successful"),
     };
 }
--- src/renderable.rs
+++ src/renderable.rs
@@ -6,260 +6,297 @@ use rand::Rng;
 use std::usize;
 
 pub struct HitRecord {
-	pub ray: Ray,
-	pub index: usize,
-	pub time: f64
+    pub ray: Ray,
+    pub index: usize,
+    pub time: f64,
 }
 
 impl HitRecord {
-	pub fn new() -> HitRecord {
-		return HitRecord {
-			ray: Ray::new(Vec3::new(0f64, 0f64, 0f64), Vec3::new(0f64, 0f64, 0f64)),
-			index: 0,
-			time: 0f64
-		};
-	}
+    pub fn new() -> HitRecord {
+        return HitRecord {
+                   ray: Ray::new(Vec3::new(0f64, 0f64, 0f64), Vec3::new(0f64, 0f64, 0f64)),
+                   index: 0,
+                   time: 0f64,
+               };
+    }
 }
 
 fn random_in_unit_sphere() -> Vec3 {
-	let u = &Vec3::new(1f64, 1f64, 1f64);
-	let mut p = Vec3::new(2f64, 2f64, 2f64);
-	while {
-		p.length_squared() >= 1.0f64
-	} {
-		let rand_x = rand::thread_rng().gen_range(0f64,1f64);
-		let rand_y = rand::thread_rng().gen_range(0f64,1f64);
-		let rand_z = rand::thread_rng().gen_range(0f64,1f64);
-		p = &(2.0f64 * &Vec3::new(rand_x, rand_y, rand_z)) - u;
-	}
-	return p;
+    let u = &Vec3::new(1f64, 1f64, 1f64);
+    let mut p = Vec3::new(2f64, 2f64, 2f64);
+    while {
+              p.length_squared() >= 1.0f64
+          } {
+        let rand_x = rand::thread_rng().gen_range(0f64, 1f64);
+        let rand_y = rand::thread_rng().gen_range(0f64, 1f64);
+        let rand_z = rand::thread_rng().gen_range(0f64, 1f64);
+        p = &(2.0f64 * &Vec3::new(rand_x, rand_y, rand_z)) - u;
+    }
+    return p;
 }
 
 pub trait Renderable {
-	// the time along the ray where the intersection occurs
-	fn get_hit_time(&self, ray: &Ray, time_min: f64, time_max: f64) -> f64;
+    // the time along the ray where the intersection occurs
+    fn get_hit_time(&self, ray: &Ray, time_min: f64, time_max: f64) -> f64;
 }
 
 // only supports spheres right now (avoid dynamic dispatch with explicit vector template)
 pub struct RenderList<'a> {
-	// list of spheres to render with parallel array
-	m_spheres: Vec<shapes::Sphere>,
-	m_sphere_materials: Vec<&'a Material>, // dynamic dispatch on materials
+    // list of spheres to render with parallel array
+    m_spheres: Vec<shapes::Sphere>,
+    m_sphere_materials: Vec<&'a Material>, // dynamic dispatch on materials
 }
 
 impl<'a> RenderList<'a> {
-		pub fn new() -> RenderList<'a> {
-			return RenderList {
-				m_spheres: Vec::new(),
-				m_sphere_materials: Vec::new()
-			};
-		}
+    pub fn new() -> RenderList<'a> {
+        return RenderList {
+                   m_spheres: Vec::new(),
+                   m_sphere_materials: Vec::new(),
+               };
+    }
 
-		//@nicco: maybe return handle/index if we need it later?
-		pub fn add_sphere(& mut self, sphere: &shapes::Sphere, material: &'a Material) {
-			//@nicco: derive copy for vec3 and sphere
-			let sphere_copy = shapes::Sphere {
-				center: Vec3::new(sphere.center.x, sphere.center.y, sphere.center.z),
-				radius: sphere.radius
-			};
-			self.m_spheres.push(sphere_copy);
-			self.m_sphere_materials.push(material);
-		}
+    //@nicco: maybe return handle/index if we need it later?
+    pub fn add_sphere(&mut self, sphere: &shapes::Sphere, material: &'a Material) {
+        //@nicco: derive copy for vec3 and sphere
+        let sphere_copy = shapes::Sphere {
+            center: Vec3::new(sphere.center.x, sphere.center.y, sphere.center.z),
+            radius: sphere.radius,
+        };
+        self.m_spheres.push(sphere_copy);
+        self.m_sphere_materials.push(material);
+    }
 
-	    pub fn try_get_hit_record(&self, ray: &Ray, time_min: f64, time_max: f64, hit_record: &mut HitRecord) -> bool {
-			let mut closest_hit_index:usize = usize::MAX;
-			let mut closest_time = time_max;
-			for index in 0..self.m_spheres.len() {
-				let sphere = &self.m_spheres[index];
-				let hit_time = sphere.get_hit_time(ray, time_min, closest_time);
-				if hit_time > time_min {
-					closest_time = hit_time;
-					closest_hit_index = index;
-				}
-			}
-			if closest_hit_index == usize::MAX {
-				return false;
-			}
-			hit_record.time = closest_time;
-			hit_record.index = closest_hit_index;
-			hit_record.ray = Ray::new(Vec3::new(ray.origin.x, ray.origin.y, ray.origin.z), Vec3::new(ray.dir.x, ray.dir.y, ray.dir.z));
-			return true;
-		}
+    pub fn try_get_hit_record(&self,
+                              ray: &Ray,
+                              time_min: f64,
+                              time_max: f64,
+                              hit_record: &mut HitRecord)
+                              -> bool {
+        let mut closest_hit_index: usize = usize::MAX;
+        let mut closest_time = time_max;
+        for index in 0..self.m_spheres.len() {
+            let sphere = &self.m_spheres[index];
+            let hit_time = sphere.get_hit_time(ray, time_min, closest_time);
+            if hit_time > time_min {
+                closest_time = hit_time;
+                closest_hit_index = index;
+            }
+        }
+        if closest_hit_index == usize::MAX {
+            return false;
+        }
+        hit_record.time = closest_time;
+        hit_record.index = closest_hit_index;
+        hit_record.ray = Ray::new(Vec3::new(ray.origin.x, ray.origin.y, ray.origin.z),
+                                  Vec3::new(ray.dir.x, ray.dir.y, ray.dir.z));
+        return true;
+    }
 
-		pub fn get_material_package(&self, ray: &Ray, hit_time: f64, hit_index:usize) -> MaterialPackage<'a> {
-				let sphere = &self.m_spheres[hit_index];
-				//@nicco: make the vec3 and ray classes implement the copy trait
-				let origin = Vec3::new(ray.origin.x, ray.origin.y, ray.origin.z);
-				let dir = Vec3::new(ray.dir.x, ray.dir.y, ray.dir.z);
-				let material_input = MaterialInput {
-					point: ray.point_at(hit_time),
-					normal: &(&ray.point_at(hit_time) - &sphere.center) / sphere.radius,
-					incoming_ray: Ray::new(origin, dir)
-				};
+    pub fn get_material_package(&self,
+                                ray: &Ray,
+                                hit_time: f64,
+                                hit_index: usize)
+                                -> MaterialPackage<'a> {
+        let sphere = &self.m_spheres[hit_index];
+        //@nicco: make the vec3 and ray classes implement the copy trait
+        let origin = Vec3::new(ray.origin.x, ray.origin.y, ray.origin.z);
+        let dir = Vec3::new(ray.dir.x, ray.dir.y, ray.dir.z);
+        let material_input = MaterialInput {
+            point: ray.point_at(hit_time),
+            normal: &(&ray.point_at(hit_time) - &sphere.center) / sphere.radius,
+            incoming_ray: Ray::new(origin, dir),
+        };
 
-				let material_package = MaterialPackage {
-					material: self.m_sphere_materials[hit_index],
-					material_input: material_input,
-				};
-				return material_package;
-		}
+        let material_package = MaterialPackage {
+            material: self.m_sphere_materials[hit_index],
+            material_input: material_input,
+        };
+        return material_package;
+    }
 }
 
 pub mod shapes {
-	use rusty_math::*;
-	use super::Renderable;
-	//
-	// Sphere
-	//
-	pub struct Sphere {
-		pub center: Vec3,
-		pub radius: f64,
-	}
+    use rusty_math::*;
+    use super::Renderable;
+    //
+    // Sphere
+    //
+    pub struct Sphere {
+        pub center: Vec3,
+        pub radius: f64,
+    }
 
-	impl Renderable for Sphere {
-		fn get_hit_time(&self, ray: &Ray, time_min: f64, time_max: f64) -> f64 {
-		    let offset_origin = &ray.origin - &self.center;
-		    let a = ray.dir.dot(&ray.dir);
-		    let b = 2f64 * offset_origin.dot(&ray.dir);
-		    let c = offset_origin.dot(&offset_origin) - self.radius * self.radius;
-		    let discriminant: f64 = &b * &b - 4f64 * &a * &c;
+    impl Renderable for Sphere {
+        fn get_hit_time(&self, ray: &Ray, time_min: f64, time_max: f64) -> f64 {
+            let offset_origin = &ray.origin - &self.center;
+            let a = ray.dir.dot(&ray.dir);
+            let b = 2f64 * offset_origin.dot(&ray.dir);
+            let c = offset_origin.dot(&offset_origin) - self.radius * self.radius;
+            let discriminant: f64 = &b * &b - 4f64 * &a * &c;
 
-			if discriminant <= 0f64 {
-				return time_min;
-			}
+            if discriminant <= 0f64 {
+                return time_min;
+            }
 
-			let discriminant_root = discriminant.sqrt();
-			let mut time: f64 = (-b - discriminant_root) / (2.0f64 * &a);
+            let discriminant_root = discriminant.sqrt();
+            let mut time: f64 = (-b - discriminant_root) / (2.0f64 * &a);
 
-			// check if in range
-			if time >= time_max || time <= time_min {
-				time = (-b + discriminant_root) / (2.0f64 * &a);
-				if time >= time_max || time <= time_min {
-					return time_min; // not in range
-				}
-			}
-			return time;
-		}
-	}
+            // check if in range
+            if time >= time_max || time <= time_min {
+                time = (-b + discriminant_root) / (2.0f64 * &a);
+                if time >= time_max || time <= time_min {
+                    return time_min; // not in range
+                }
+            }
+            return time;
+        }
+    }
 }
 
 pub struct MaterialInput {
-	pub incoming_ray: Ray,
-	pub point: Vec3,
-	pub normal: Vec3,
+    pub incoming_ray: Ray,
+    pub point: Vec3,
+    pub normal: Vec3,
 }
 
 pub struct MaterialOutput {
-	pub attenuation:Vec3,
-	pub scattered:Ray,
+    pub attenuation: Vec3,
+    pub scattered: Ray,
 }
 
 impl MaterialOutput {
-	pub fn new() -> MaterialOutput {
-		return MaterialOutput {
-			attenuation: Vec3::new(0f64, 0f64, 0f64),
-			scattered: Ray::new(Vec3::new(0f64, 0f64, 0f64), Vec3::new(0f64, 0f64, 0f64))
-		};
-	}
+    pub fn new() -> MaterialOutput {
+        return MaterialOutput {
+                   attenuation: Vec3::new(0f64, 0f64, 0f64),
+                   scattered: Ray::new(Vec3::new(0f64, 0f64, 0f64), Vec3::new(0f64, 0f64, 0f64)),
+               };
+    }
 }
 
 pub struct MaterialPackage<'a> {
-	pub material: &'a Material,
-	pub material_input: MaterialInput,
+    pub material: &'a Material,
+    pub material_input: MaterialInput,
 }
 
 pub trait Material {
-	fn apply(&self, input: &MaterialInput, output: &mut MaterialOutput) -> bool;
+    fn apply(&self, input: &MaterialInput, output: &mut MaterialOutput) -> bool;
 }
 
 pub mod materials {
-	use rusty_math::*;
-	use super::Material;
-	use super::MaterialInput;
-	use super::MaterialOutput;
+    extern crate rand;
+    use rusty_math::*;
+    use super::Material;
+    use super::MaterialInput;
+    use super::MaterialOutput;
+    use rand::Rng;
 
-	pub struct Lambertian {
-		pub albedo:Vec3,
-	}
+    pub struct Lambertian {
+        pub albedo: Vec3,
+    }
 
-	impl Material for Lambertian {
-		fn apply(&self, input: &MaterialInput, output: &mut MaterialOutput) -> bool {
-			let target = &(&input.point + &input.normal) + &super::random_in_unit_sphere();
-			let dir = &target - &input.point;
+    impl Material for Lambertian {
+        fn apply(&self, input: &MaterialInput, output: &mut MaterialOutput) -> bool {
+            let target = &(&input.point + &input.normal) + &super::random_in_unit_sphere();
+            let dir = &target - &input.point;
 
-			output.scattered = Ray::new(Vec3::new(input.point.x, input.point.y, input.point.z), dir);
-			output.attenuation = Vec3::new(self.albedo.x, self.albedo.y, self.albedo.z);
+            output.scattered = Ray::new(Vec3::new(input.point.x, input.point.y, input.point.z),
+                                        dir);
+            output.attenuation = Vec3::new(self.albedo.x, self.albedo.y, self.albedo.z);
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 
-	pub struct Metal {
-		pub albedo:Vec3,
-		pub fuzziness:f64,
-	}
+    pub struct Metal {
+        pub albedo: Vec3,
+        pub fuzziness: f64,
+    }
 
-	impl Material for Metal {
-		fn apply(&self, input: &MaterialInput, output: &mut MaterialOutput) -> bool {
-			// get reflected
-			let unit_dir = &(input.incoming_ray.dir.normalize());
-			let surface_normal = &input.normal;
-			let mut reflected = unit_dir - &(2f64 * unit_dir.dot(&surface_normal) * surface_normal);
-			reflected = &reflected + &(self.fuzziness * &super::random_in_unit_sphere());
+    impl Material for Metal {
+        fn apply(&self, input: &MaterialInput, output: &mut MaterialOutput) -> bool {
+            // get reflected
+            let unit_dir = &(input.incoming_ray.dir.normalize());
+            let surface_normal = &input.normal;
+            let mut reflected = unit_dir - &(2f64 * unit_dir.dot(&surface_normal) * surface_normal);
+            reflected = &reflected + &(self.fuzziness * &super::random_in_unit_sphere());
 
-			if reflected.dot(surface_normal) > 0f64 {
-				let hit_point = Vec3::new(input.point.x, input.point.y, input.point.z);
-				output.scattered = Ray::new(hit_point, reflected);
-				output.attenuation = Vec3::new(self.albedo.x, self.albedo.y, self.albedo.z);
-				return true;
-			}
-			return false;
-		}
-	}
+            if reflected.dot(surface_normal) > 0f64 {
+                let hit_point = Vec3::new(input.point.x, input.point.y, input.point.z);
+                output.scattered = Ray::new(hit_point, reflected);
+                output.attenuation = Vec3::new(self.albedo.x, self.albedo.y, self.albedo.z);
+                return true;
+            }
+            return false;
+        }
+    }
 
+    pub struct Dielectric {
+        pub refraction_index: f64,
+    }
 
-	pub struct Dielectric {
-		pub refractionIndex:f64,
-	}
+    fn schlick(cosine: f64, refraction_index: f64) -> f64 {
+        let mut r0 = (1f64 - refraction_index) / (1f64 + refraction_index);
+        r0 *= r0;
+        return r0 + (1f64 - r0) * (1f64 - cosine).powf(5f64);
+    }
 
-	impl Material for Dielectric {
-		fn apply(&self, input: &MaterialInput, output: &mut MaterialOutput) -> bool {
-			let mut outward_normal:Vec3;
+    impl Material for Dielectric {
+        fn apply(&self, input: &MaterialInput, output: &mut MaterialOutput) -> bool {
+            let outward_normal: Vec3;
 
-			// get reflected
-			let unit_dir = &(input.incoming_ray.dir.normalize());
-			let surface_normal = &input.normal;
-			let mut reflected = unit_dir - &(2f64 * unit_dir.dot(&surface_normal) * surface_normal);
-			let mut niOverNt:f64 = 0f64;
+            // get reflected
+            let dir = &(input.incoming_ray.dir);
+            let dir_normalize = dir.normalize();
+            let surface_normal = &input.normal;
 
-			output.attenuation = Vec3::new(1f64, 1f64, 1f64);
+            let reflected = &dir_normalize -
+                            &(2f64 * dir_normalize.dot(&surface_normal) * surface_normal);
+            let ni_over_nt: f64;
+            let cosine: f64;
+            let reflect_chance: f64;
 
-			if unit_dir.dot(surface_normal) > 0f64 {
-				outward_normal = -1f64 * surface_normal;
-				niOverNt = self.refractionIndex;
-			}
-			else {
-				outward_normal = Vec3::new(surface_normal.x, surface_normal.y, surface_normal.z);
-				niOverNt = 1.0f64 / self.refractionIndex;
-			}
+            output.attenuation = Vec3::new(1f64, 1f64, 1f64);
 
-			let hit_point = Vec3::new(input.point.x, input.point.y, input.point.z);
+            let dir_dot_normal = dir.dot(&input.normal);
+            let dir_length = dir.length_squared().sqrt();
 
-			// calculate refraction
-			// ray dir, outward_normal, niOverNt, refracted
-			let dt = unit_dir.dot(&outward_normal);
-			let discriminant = 1f64 - niOverNt * niOverNt * (1f64 - dt * dt);
-			if discriminant > 0f64 { // fuck this fucking syntax
-				let refracted = &(niOverNt * &(unit_dir - &(dt * &outward_normal))) - &(&outward_normal * discriminant.sqrt());
-				output.scattered = Ray::new(hit_point, refracted);
-			}
-			else {
-				output.scattered = Ray::new(hit_point, reflected);
-				return false;
-			}
+            if dir_dot_normal > 0f64 {
+                outward_normal = -1f64 * surface_normal;
+                ni_over_nt = self.refraction_index;
+                cosine = self.refraction_index * dir_dot_normal / dir_length;
+            } else {
+                outward_normal = Vec3::new(surface_normal.x, surface_normal.y, surface_normal.z);
+                ni_over_nt = 1.0f64 / self.refraction_index;
+                cosine = -dir_dot_normal / dir_length;
+            }
 
-			return true;
-		}
-	}
+            let hit_point = Vec3::new(input.point.x, input.point.y, input.point.z);
+
+            // calculate refraction
+            // ray dir, outward_normal, ni_over_nt, refracted
+            let dt = dir_normalize.dot(&outward_normal);
+            let discriminant = 1f64 - ni_over_nt * ni_over_nt * (1f64 - dt * dt);
+            let mut refracted: Vec3 = Vec3::new(0f64, 0f64, 0f64);
+            if discriminant > 0f64 {
+                // fuck this syntax
+                refracted = &(ni_over_nt * &(&dir_normalize - &(dt * &outward_normal))) -
+                            &(&outward_normal * discriminant.sqrt());
+                reflect_chance = schlick(cosine, self.refraction_index);
+            } else {
+                // we reflect
+                reflect_chance = 1f64;
+            }
+
+            // roll to see if we reflect
+            let roll = rand::thread_rng().gen_range(0f64, 1f64);
+            if roll < reflect_chance {
+                output.scattered = Ray::new(hit_point, reflected);
+            } else {
+                output.scattered = Ray::new(hit_point, refracted);
+            }
+
+            return true;
+        }
+    }
 
 }
